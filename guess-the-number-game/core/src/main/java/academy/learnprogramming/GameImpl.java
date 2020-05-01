5ce661b1375655b1a105ab5334db69319a46a5e0
David Yeshi <davidyeshi0@gmail.com>
2020-05-01 15:57:03 +1000

    Using javax annotations for invoking init and destroy methods

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/GameImpl.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -3,6 +3,9 @@ package academy.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class GameImpl implements Game {
 
     // == constants ==
@@ -18,13 +21,8 @@ public class GameImpl implements Game {
     private int remainingGuesses;
     private boolean validNumberRange = true;
 
-    // == public methods ==
-
-    // setter injection dependency
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
-
+    // == init ==
+    @PostConstruct
     @Override
     public void reset() {
         smallest = 0;
@@ -33,6 +31,18 @@ public class GameImpl implements Game {
         biggest = numberGenerator.getMaxNumber();
         number = numberGenerator.next();
         log.debug("The number is {}", number);
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.info("in Game preDestroy()");
+    }
+
+    // == public methods ==
+
+    // setter injection dependency
+    public void setNumberGenerator(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
     }
 
     @Override
