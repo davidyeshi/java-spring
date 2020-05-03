88a9afcc993a1eb3d928e172097bb3c4849bad71
David Yeshi <davidyeshi0@gmail.com>
2020-05-03 12:56:24 +1000

    Using annotations for event listener

--- guess-the-number-game/console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
+++ guess-the-number-game/console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
@@ -4,17 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class ConsoleNumberGuess {
 
     // == constants ==
     private static final Logger logger = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
     // Once the Spring container is ready for use, it will fire this event automatically
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        logger.info("Container ready for use.");
+    @EventListener
+    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+        logger.info("start() --> Container ready for use.");
     }
 }
