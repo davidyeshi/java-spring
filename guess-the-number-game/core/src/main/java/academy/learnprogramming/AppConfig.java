4d23fb3be30bdc563a5c4873f7374236bb6619c8
David Yeshi <davidyeshi0@gmail.com>
2020-05-03 11:06:25 +1000

    Using bean methods and remove Component annotation from class

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/AppConfig.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/AppConfig.java
@@ -1,9 +1,22 @@
 package academy.learnprogramming;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "academy.learnprogramming")
 public class AppConfig {
+    // define definitions to find the beans
+    // Bean methods are useful in situations where need additional config for a bean
+    // == bean methods to be managed by the spring container ==
+    @Bean
+    public NumberGenerator numberGenerator() {
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game() {
+        return new GameImpl();
+    }
 }
