68de0d7246d74abf2395d84ece01abbe8a1699b9
David Yeshi <davidyeshi0@gmail.com>
2020-05-03 13:09:43 +1000

    Finish game logic

--- guess-the-number-game/console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
+++ guess-the-number-game/console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
@@ -1,11 +1,16 @@
 package academy.learnprogramming.console;
 
+import academy.learnprogramming.Game;
+import academy.learnprogramming.MessageGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
 
 @Component
 public class ConsoleNumberGuess {
@@ -13,9 +18,40 @@ public class ConsoleNumberGuess {
     // == constants ==
     private static final Logger logger = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
+    // == fields ==
+    @Autowired
+    private Game game;
+
+    @Autowired
+    private MessageGenerator messageGenerator;
+
+    // == events ==
     // Once the Spring container is ready for use, it will fire this event automatically
     @EventListener
     public void start(ContextRefreshedEvent contextRefreshedEvent) {
         logger.info("start() --> Container ready for use.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            int guess = scanner.nextInt();
+            scanner.nextLine();
+            game.setGuess(guess);
+            game.check();
+
+            if (game.isGameWon() || game.isGameLost()) {
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("Play again y/n?");
+                String playAgain = scanner.nextLine().trim();
+                if(!playAgain.equalsIgnoreCase("y")) {
+                    break;
+                }
+
+                game.reset();
+            }
+        }
     }
 }
