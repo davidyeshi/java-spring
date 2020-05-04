9a8cbf7d950f98950f904928a0ba7774615f260b
David Yeshi <davidyeshi0@gmail.com>
2020-05-05 07:46:39 +1000

    Inject values from a properties file

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/config/GameConfig.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -2,15 +2,21 @@ package academy.learnprogramming.config;
 
 import academy.learnprogramming.GuessCount;
 import academy.learnprogramming.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 100;
-    private int guessCount = 10;
+    @Value("${game.maxNumber}")
+    private int maxNumber;
+
+    @Value("${game.guessCount}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
