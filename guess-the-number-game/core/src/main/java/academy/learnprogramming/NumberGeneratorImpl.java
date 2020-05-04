384f305f4e8bcaa22352c0810c28684ba4546dd9
David Yeshi <davidyeshi0@gmail.com>
2020-05-05 08:17:13 +1000

    Using @Component rather then bean methods to instantiate beans

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Component
 public class NumberGeneratorImpl implements NumberGenerator {
 
     // == fields ==
--- guess-the-number-game/core/src/main/java/academy/learnprogramming/config/AppConfig.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/config/AppConfig.java
@@ -10,13 +10,6 @@ import org.springframework.context.annotation.Import;
 @Import(GameConfig.class) // allows modularised configuration
 @ComponentScan(basePackages = "academy.learnprogramming")
 public class AppConfig {
-    // define definitions to find the beans
-    // Bean methods are useful in situations where need additional config for a bean
-    // == bean methods to be managed by the spring container ==
-    @Bean
-    public NumberGenerator numberGenerator() {
-        return new NumberGeneratorImpl();
-    }
 
     @Bean
     public Game game() {
