413a459973cccf1b6dbc4328247187a80ac21dda
David Yeshi <davidyeshi0@gmail.com>
2020-05-05 10:23:02 +1000

    Revert "Lombok fix"
    This reverts commit d8297bb782e66b704bc63bee441efb359a23c6a6.

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -22,7 +22,7 @@ public class MessageGeneratorImpl implements MessageGenerator{
     // == init methods ==
     @PostConstruct
     public void postContruct() {
-        log.info("in MessageGeneratorImpl.postConstruct() {}", game);
+        logger.info("in MessageGeneratorImpl.postConstruct() {}", game);
     }
 
     // == public methods ==
