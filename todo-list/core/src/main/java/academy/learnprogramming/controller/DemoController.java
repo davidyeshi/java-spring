2c6fd7775cf8a04cb103098c754410951b141e63
David Yeshi <davidyeshi0@gmail.com>
2020-05-05 16:24:56 +1000

    Model and Model Attributes

--- todo-list/core/src/main/java/academy/learnprogramming/controller/DemoController.java
+++ todo-list/core/src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,10 +1,14 @@
 package academy.learnprogramming.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class DemoController {
 
     // http://localhost:8080/core/hello
@@ -15,10 +19,20 @@ public class DemoController {
     }
 
     // http://localhost:8080/core/welcome
-    // prefix + name + suffix
-    // /WEB-INF/view/welcome.jsp
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+
+        model.addAttribute("user","David");
+        log.info("model = {}", model);
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
         return "welcome";
+    }
+
+    // model attribute is added to the model by Spring
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage(){
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo application.";
     }
 }
