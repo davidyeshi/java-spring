ed5065caa418c4d5541013a7173861fadef779a7
David Yeshi <davidyeshi0@gmail.com>
2020-05-08 09:57:16 +1000

    Added home view and navigation url

--- todo-list/core/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ todo-list/core/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,10 +1,13 @@
 package academy.learnprogramming.config;
 
+import academy.learnprogramming.util.ViewNames;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
@@ -14,7 +17,7 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @Configuration
 @ComponentScan(basePackages = "academy.learnprogramming")
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     // == constants ==
     public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
@@ -29,5 +32,12 @@ public class WebConfig {
         viewResolver.setSuffix(RESOLVER_SUFFIX);
 
         return viewResolver;
+    }
+
+    // Adding the view controller which is an automated controller which is pre configured
+    // and directs it to the home page
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME);
     }
 }
