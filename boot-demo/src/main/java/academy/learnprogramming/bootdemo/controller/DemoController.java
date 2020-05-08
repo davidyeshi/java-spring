939b952e2aecef5874314cafd6adfb089903928a
David Yeshi <davidyeshi0@gmail.com>
2020-05-08 12:53:34 +1000

    Demo Welcome View

--- boot-demo/src/main/java/academy/learnprogramming/bootdemo/controller/DemoController.java
+++ boot-demo/src/main/java/academy/learnprogramming/bootdemo/controller/DemoController.java
@@ -2,6 +2,7 @@ package academy.learnprogramming.bootdemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,5 +16,12 @@ public class DemoController {
     public String demo() {
         log.info("demo method called");
         return "Hello Spring boot";
+    }
+
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+        log.info("welcome method called");
+        model.addAttribute("message","Welcome to Spring boot");
+        return "welcome";
     }
 }
