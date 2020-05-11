ffe9f45627e24a5f01bc65d9c76e3603dbc55686
David Yeshi <davidyeshi0@gmail.com>
2020-05-11 10:50:25 +1000

    Request Interception

--- guess-the-number-game/web/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ guess-the-number-game/web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,7 +1,9 @@
 package academy.learnprogramming.config;
 
+import academy.learnprogramming.interceptor.RequestInterceptor;
 import academy.learnprogramming.util.ViewNames;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +16,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName(ViewNames.HOME);
         registry.addViewController("home").setViewName(ViewNames.HOME);
 
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
